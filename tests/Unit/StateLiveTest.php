1a6ed983cb02c3af978c92d68dd2fde2221cac59
Joo Martin <joomartin@jmweb.hu>
2017-02-01 12:36:29 +0100

    Elo sejtek tesztje

--- tests/Unit/StateLiveTest.php
+++ tests/Unit/StateLiveTest.php
@@ -9,20 +9,76 @@ use App\Cell\StateLive;
 
 class StateLiveTest extends TestCase
 {
-    public function testNextState()
+    /**
+     * Szabalyok:
+     *  1. Tovabb el, ha 2 vagy 3 szomszedja van
+     *  2. Meghal, ha 1 elo szomszedja van, VAGY 3 -nal tobb elo szomszedja van
+     */
+
+    /**
+     * Tovabb el az 1. szabaly alapjan
+     * @covers StateLive::nextState()
+     */
+    public function testLiveStateStayingAliveByTwoNeighbours()
     {
-        // [1][1] pozicioju elem szomszedjai
-        /* $counterHorizontal = new NeighbourCounterHorizontal($cells, 1, 1); */
-        $counters = $this->getCounters(['horizontal' => 1, 'vertical' => 1, 'diagonal' => 1], [], 1, 1);
         $state = new StateLive(1);
+
+        // Osszesen 2 szomszedja van
+        $counters = $this->getCounters(['horizontal' => 1, 'vertical' => 1, 'diagonal' => 0], [], 1, 1);
         $nextState = $state->nextState([], $counters);
 
+        $this->assertEquals(1, $nextState);
     }
 
-    protected function getCounters(array $counts, array $cells, $x, $y)
+    /**
+     * Tovabb el az 1. szabaly alapjan
+     * @covers StateLive::nextState()
+     */
+    public function testLiveStateStayingAliveByThreeNeighbours()
+    {
+        $state = new StateLive(1);
+
+        // Osszesen 3 szomszedja van
+        $counters = $this->getCounters(['horizontal' => 1, 'vertical' => 1, 'diagonal' => 1]);
+        $nextState = $state->nextState([], $counters);
+
+        $this->assertEquals(1, $nextState);
+    }
+
+    /**
+     * Elpusztul a 2. szabaly alapjan
+     * @covers StateLive::nextState()
+     */
+    public function testLiveStateDieByOneNeighbours()
+    {
+        $state = new StateLive(1);
+
+        // Osszesen 1 szomszedja van
+        $counters = $this->getCounters(['horizontal' => 1, 'vertical' => 0, 'diagonal' => 0]);
+        $nextState = $state->nextState([], $counters);
+
+        $this->assertEquals(0, $nextState);
+    }
+
+    /**
+     * Elpusztul a 2. szabaly alapjan
+     * @covers StateLive::nextState()
+     */
+    public function testLiveStateDieByTooMuchNeighbours()
+    {
+        $state = new StateLive(1);
+
+        // Osszesen 4 szomszedja van
+        $counters = $this->getCounters(['horizontal' => 2, 'vertical' => 1, 'diagonal' => 1]);
+        $nextState = $state->nextState([], $counters);
+
+        $this->assertEquals(0, $nextState);
+    }
+
+    protected function getCounters(array $counts)
     {
         $mockHorizontal = $this->getMockBuilder('App\Board\NeighbourCounterHorizontal')
-            ->setConstructorArgs([$cells, $x, $y])
+            ->setConstructorArgs([[], 1, 1])
             ->setMethods(['getCount'])
             ->getMock();
 
@@ -31,7 +87,7 @@ class StateLiveTest extends TestCase
             ->will($this->returnValue($counts['horizontal']));
 
         $mockVertical = $this->getMockBuilder('App\Board\NeighbourCounterVertical')
-            ->setConstructorArgs([$cells, $x, $y])
+            ->setConstructorArgs([[], 1, 1])
             ->setMethods(['getCount'])
             ->getMock();
 
@@ -40,7 +96,7 @@ class StateLiveTest extends TestCase
             ->will($this->returnValue($counts['vertical']));
 
         $mockDiagonal = $this->getMockBuilder('App\Board\NeighbourCounterDiagonal')
-            ->setConstructorArgs([$cells, $x, $y])
+            ->setConstructorArgs([[], 1, 1])
             ->setMethods(['getCount'])
             ->getMock();
 
